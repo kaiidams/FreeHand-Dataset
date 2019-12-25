6afdbb953d52db6aed26f1512b7ab5671cd6c19a
Katsuya Iida <katsuya.iida@gmail.com>
2019-12-25 12:42:41 +0900

    Wrist movement

--- render_batch.py
+++ render_batch.py
@@ -9,9 +9,9 @@ import numpy as np
 
 
 BONE_NAMES = (
-    'Camera',
-    'Camera',
-    'Camera',
+    'Camera',  # elevation
+    'Camera',  # azmith
+    'Camera',  # roll
     'wrist.R', # hori
     'wrist.R', # vert
     'finger1.R',
@@ -38,7 +38,7 @@ BONE_RAND_RANGE = np.array([
     90,
     360,
     360,
-    -10,
+    10,
     30,
     60,
     60,
@@ -51,7 +51,7 @@ BONE_MAX = np.array([
     90,
     360,
     360,
-    -10,
+    10,
     30,
     40,
     40,
@@ -61,6 +61,16 @@ BONE_MAX = np.array([
     ])
     
 
+def setup():
+    '''Changes to the POSE mode.'''
+    view_layer = bpy.context.view_layer
+    ob = bpy.data.objects['Hand']
+    ob.select_set(True)
+    view_layer.objects.active = ob
+    # bpy.context.scene.objects.active = ob # 2.7x
+    bpy.ops.object.mode_set(mode='POSE')
+    
+    
 def random_angles():
     '''Returns random angles as a numpy array.'''
     angles = np.random.random(len(BONE_NAMES)) * BONE_RAND_RANGE + BONE_MIN
@@ -78,56 +88,63 @@ def random_angles():
 
 def apply_handpose(angles):
     '''Applies angles to the hand bones.'''
-    o = bpy.data.objects['Hand']
-    for i in range(5, 10):
+    ob = bpy.data.objects['Hand']
+    for i in range(3, 10):
         bonename = BONE_NAMES[i]
-        bone = o.pose.bones[bonename]
+        bone = ob.pose.bones[bonename]
         angle = angles[i]
-        bone.rotation_quaternion.x = angle * pi / 180
+        if i == 4: # vert
+            bone.rotation_quaternion.z = angle * pi / 180
+        else:
+            bone.rotation_quaternion.x = angle * pi / 180
 
 
-def render_once(index):
-    
-    angles = random_angles()
-    print(angles)
-    apply_handpose(angles)
-
-    scene = bpy.data.scenes['Scene']
+def apply_camerapose(angles, camera_distance=1.0):
     camera_object = bpy.data.objects['Camera']
+
+    elevation_angle = angles[0] * pi / 180
+    azmith_angle = angles[1] * pi / 180
+    roll_angle = angles[2] * pi / 180
     
-    elevation_angle = random.uniform(-30, 90) * pi / 180
-    azmith_angle = random.uniform(0, 360) * pi / 180
-    yaw_angle = random.uniform(0, 360) * pi / 180
-    camera_distance = 1.0
-    
-    print([x for x in dir(camera_object) if 'rot' in x])
-    print(camera_object.rotation_euler)
-    
-    camera_object.rotation_euler.x = - elevation_angle + pi / 2
-    camera_object.rotation_euler.y = 0 #yaw_angle
+    camera_object.rotation_euler.x = -elevation_angle + pi / 2
+    camera_object.rotation_euler.y = 0.0
     camera_object.rotation_euler.z = azmith_angle + pi / 2
 
     camera_pos = (
         camera_distance * cos(azmith_angle) * cos(elevation_angle),
         camera_distance * sin(azmith_angle) * cos(elevation_angle),
-        camera_distance * sin(elevation_angle)
+        camera_distance * sin(elevation_angle) + 0.07
         )
     camera_object.location.x = camera_pos[0]
     camera_object.location.y = camera_pos[1]
-    camera_object.location.z = camera_pos[2] + 0.07
+    camera_object.location.z = camera_pos[2]
     
+
+def apply_lights():
+    ob = bpy.data.objects['Hand']
+    
+
+def render_scene(index):
+    scene = bpy.data.scenes['Scene']
     fname = '{0:05d}.png'.format(index)
-    print(fname)
     bpy.context.scene.render.filepath = os.path.join(os.getcwd(), 'images', fname)
     bpy.ops.render.render(write_still=True)
+
+
+def process_once(index):
+    print('Processing scene {}'.format(index))
+    angles = random_angles()
+    apply_handpose(angles)
+    apply_camerapose(angles)
+    apply_lights()
+    #render_scene(index)
     
     
-if __name__ == '__main__':
-    view_layer = bpy.context.view_layer
-    ob = bpy.data.objects['Hand']
-    ob.select_set(True)
-    view_layer.objects.active = ob
-    # bpy.context.scene.objects.active = ob # 2.7x
-    bpy.ops.object.mode_set(mode='POSE')
+def main():
+    setup()
     for i in range(10):
-        render_once(i)
+        process_once(i)
+
+        
+if __name__ == '__main__':
+    main()
